be681755636911f4c9c2f8226c7d47627b38056a
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-07-13 21:40:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,7 @@ export default function Home() {
                     Welcome to Shri Ram Centennial School
                 </h1>
                 <p>
-                    Shri Ram Centennial School, Patna was founded in the year 2014. It is an initiative of the Shri Ram
-                    Education Trust, an institution conceived to empower the best education, coupled with development of
-                    values and personality traits, that breeds achievers. Affiliated to CBSE, offering K-12 education,
-                    we aim at shaping the future of Bihar so that our students can take the challenges of the world head
-                    on. We believe that learning must lead to awareness and recognition of one's individual
-                    sensibilities and to the development and moulding of character, whereby one's dignity and self-worth
-                    is enhanced. We emphasize on- Activity Based Teaching, English Speaking Skills, Personality
-                    Development Classes, focusing on the Holistic Development of the child . We have a child- friendly
-                    environment and child safety is our utmost priority.
+                    Shri Ram Centennial School, Patna was founded in the year 2014. It is an initiative of the Shri Ram Education Trust, an institution conceived to empower the best education, coupled with development of values and personality traits, that breeds achievers. Affiliated to CBSE, offering K-12 education, we aim at shaping the future of Bihar so that our students can take the challenges of the world head on. We believe that learning must lead to awareness and recognition of one's individual sensibilities and to the development and moulding of character, whereby one's dignity and self-worth is enhanced. We emphasize on- Activity Based Teaching, English Speaking Skills, Personality Development Classes, focusing on the Holistic Development of the child . We have a child- friendly environment and child safety is our utmost priority.
                 </p>
             </div>
             <div>
