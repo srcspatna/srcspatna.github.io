f78308b37ffe71411ff0854baf27e511ef9147eb
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-07-13 21:44:16 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image'; // <-- Import Image component
+
 export default function Home() {
     return (
         <main className="text-justify">
@@ -6,7 +12,7 @@ export default function Home() {
                     Welcome to Shri Ram Centennial School
                 </h1>
                 <p>
-                    Shri Ram Centennial School, Patna was founded in the year 2014. It is an initiative of the Shri Ram Education Trust, an institution conceived to empower the best education, coupled with development of values and personality traits, that breeds achievers. Affiliated to CBSE, offering K-12 education, we aim at shaping the future of Bihar so that our students can take the challenges of the world head on. We believe that learning must lead to awareness and recognition of one's individual sensibilities and to the development and moulding of character, whereby one's dignity and self-worth is enhanced. We emphasize on- Activity Based Teaching, English Speaking Skills, Personality Development Classes, focusing on the Holistic Development of the child . We have a child- friendly environment and child safety is our utmost priority.
+                    Shri Ram Centennial School, Patna was founded in the year 2014. It is an initiative of the Shri Ram Education Trust, an institution conceived to empower the best education, coupled with development of values and personality traits, that breeds achievers. Affiliated to CBSE, offering K-12 education, we aim at shaping the future of Bihar so that our students can take the challenges of the world head on. We believe that learning must lead to awareness and recognition of one&apos;s individual sensibilities and to the development and moulding of character, whereby one&apos;s dignity and self-worth is enhanced. We emphasize on- Activity Based Teaching, English Speaking Skills, Personality Development Classes, focusing on the Holistic Development of the child . We have a child- friendly environment and child safety is our utmost priority.
                 </p>
             </div>
             <div>
