2b663de0dd07c5132744a83003e4b40e025d0c0d
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-07-13 21:30:52 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,31 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <Link href="//srcspatna.com" className="border p-5 rounded-2xl">
-                        <Image src="/logo.png" alt="My Image" width={150} height={150} className="w-full h-auto relative z-0 rounded-lg transition-all duration-300 hover:scale-110" />
-                    </Link>
-                </div>
-            </main>
+        <main className="text-justify">
+            <div>
+                <h1 className="text-white bg-purple-900 p-5 text-center">
+                    Welcome to Shri Ram Centennial School
+                </h1>
+                <p>
+                    Shri Ram Centennial School, Patna was founded in the year 2014. It is an initiative of the Shri Ram
+                    Education Trust, an institution conceived to empower the best education, coupled with development of
+                    values and personality traits, that breeds achievers. Affiliated to CBSE, offering K-12 education,
+                    we aim at shaping the future of Bihar so that our students can take the challenges of the world head
+                    on. We believe that learning must lead to awareness and recognition of one's individual
+                    sensibilities and to the development and moulding of character, whereby one's dignity and self-worth
+                    is enhanced. We emphasize on- Activity Based Teaching, English Speaking Skills, Personality
+                    Development Classes, focusing on the Holistic Development of the child . We have a child- friendly
+                    environment and child safety is our utmost priority.
+                </p>
+            </div>
+            <div>
+                <h2 className="text-white bg-purple-900 p-5">
+                    ABOUT SCHOOL
+                </h2>
+                <p>
+                    Legacy of 95 years of excellence in the domain of education reflected in the world class premier institutions of higher learning by Shri Ram family - SRCC & LSR. Ranked as one of the the Best CBSE Schools in Bihar by the EW Magazine, SRCS focuses on the holistic growth of the child. We cater to learning upto senior secondary level and provide career counselling as well.
+                </p>
+            </div>
+        </main>
     );
 }
