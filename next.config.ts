0a771bb3b3a235c58ac49b31c6881955a9db142d
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-07-14 14:48:59 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // Essential for GitHub Pages (static HTML export)
+    basePath: '/srcspatna.github.io', // <--- Add this line!
+    images: {
+        unoptimized: true // Optional: Can avoid image optimization issues with static export
+    }
 };
 
 export default nextConfig;
