4338b78345493737f0669000f096ae035296984a
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2026-02-25 09:55:57 +0530

    astro

--- src/app/pages/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <main className="text-justify">
-                <h1 className="text-white bg-purple-900 p-5">
-                    Pages
-                </h1>
-        </main>
-    );
-}
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-gray-800 p-4 text-white text-center text-sm mt-auto">
-            <div className="container mx-auto">
-                &copy; {new Date().getFullYear()} SRCS. All rights reserved.
-            </div>
-        </footer>
-    );
-}
